c2e93520fde7a52d92bdd7dbb83e75a98e54918a
jojohahana <yohana.chriezt@gmail.com>
2022-08-03 13:44:46 +0700

    add plugin flowbite - create date picker form

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: [
+    'index.html',
+    './node_modules/flowbite/**/*.js'],
   theme: {
     container: {
       center: true,
@@ -18,5 +20,7 @@ module.exports = {
       // },
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
